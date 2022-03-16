88c17d7e529ede0bc91f385475bf416e41518cb0
Julio <torrresmatos.julio@gmail.com>
2022-03-15 23:54:29 -0500

    First Commit py file

---
+++ calc.py
@@ -0,0 +1,31 @@
+from lib2to3.pgen2.token import EQUAL
+
+
+def main():
+    sum = 0
+    oper = 0
+    print("Simpel Paython Calculator")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    oper = int(input("Enter 1(+)2(-)3(*)4(/): "))
+    if oper ==1:
+        answer = num1+num2
+        print("answer",answer)
+
+    elif oper == 2:
+        answer = num1-num2
+        print("answer",answer)
+
+    elif oper == 3:
+        answer = num1*num2
+        print("answer",answer)
+
+    elif oper == 4:
+        answer = num1/num2
+        print("answer",answer)
+
+main()
+
+
+
+
